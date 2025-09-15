61eb2293c1c90c8dd8d880c92123e6b7553260d3
penguintwist <adamdavies928@gmail.com>
2025-09-15 20:23:37 +0100

    Update python-interpreter-clean.js

--- shared/python-interpreter-clean.js
+++ shared/python-interpreter-clean.js
@@ -301,7 +301,7 @@
         var textarea = container.querySelector('.code-editor');
         
         // Try to use CodeMirror if available
-        if (false && window.CodeMirror) {
+        if (window.CodeMirror) {
             this.editor = window.CodeMirror.fromTextArea(textarea, {
                 mode: 'python',
                 theme: 'monokai',
